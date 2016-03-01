0516ac8716f9bb02539bf13016b1106900ad2b5f
t0pth4t <t0pth4t@yahoo.com>
2016-02-29 20:16:34 -0600

    Putting in work

--- client/app/request/requests/requests.controller.js
+++ client/app/request/requests/requests.controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
+class RequestsCtrl {
+  constructor(Auth,$http, $state) {
+    this.requests = [];
+
+    this.Auth = Auth;
+    this.$state = $state;
+    this.$http = $http;
+
+    $http.get('/api/requests').then(response => {
+      this.requests = response.data;
+    });
+  }
+
+}
+
 angular.module('tryagainApp')
-  .controller('RequestsCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('RequestsCtrl', RequestsCtrl);
