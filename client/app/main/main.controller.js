3c425dba8ea8e0ec2805aefab3e23a754e51c7c0
t0pth4t <t0pth4t@yahoo.com>
2016-03-03 18:57:48 -0600

    Styles

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -6,25 +6,11 @@ class MainController {
 
   constructor(Auth, $http) {
     this.$http = $http;
-    this.awesomeThings = [];
     this.isLoggedIn = Auth.isLoggedIn;
     this.user = Auth.getCurrentUser;
 
-    $http.get('/api/things').then(response => {
-      this.awesomeThings = response.data;
-    });
   }
 
-  addThing() {
-    if (this.newThing) {
-      this.$http.post('/api/things', { name: this.newThing });
-      this.newThing = '';
-    }
-  }
-
-  deleteThing(thing) {
-    this.$http.delete('/api/things/' + thing._id);
-  }
 }
 
 angular.module('tryagainApp')
